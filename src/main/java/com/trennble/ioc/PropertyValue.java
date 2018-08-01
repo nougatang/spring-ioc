e0acc5276e8e573103957306c4040f9efdfabeb4
jiangbo <bo.jiang@iwellmass.com>
2018-08-01 11:35:00 +0800

    apply bean propertys

---
+++ src/main/java/com/trennble/ioc/PropertyValue.java
@@ -0,0 +1,29 @@
+package com.trennble.ioc;
+
+public class PropertyValue {
+
+    private String name;
+
+    private String value;
+
+    public PropertyValue(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/com/trennble/ioc/factory/AutowireCapableBeanFactory.java
+++ src/main/java/com/trennble/ioc/factory/AutowireCapableBeanFactory.java
@@ -1,17 +1,34 @@
 package com.trennble.ioc.factory;
 
 import com.trennble.ioc.BeanDefinition;
+import com.trennble.ioc.PropertyValue;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 public class AutowireCapableBeanFactory extends AbstractBeanFactory {
 
     protected Object doCreateBean(BeanDefinition beanDefinition) {
         try {
-            return beanDefinition.getBeanClass().newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
+            Object bean = createBeanInstance(beanDefinition);
+            applyPropertyValues(bean, beanDefinition);
+            return bean;
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    protected Object createBeanInstance(BeanDefinition beanDefinition) throws Exception {
+        return beanDefinition.getBeanClass().newInstance();
+    }
+
+    protected void applyPropertyValues(Object bean, BeanDefinition beanDefinition) throws Exception {
+        List<PropertyValue> propertyValues = beanDefinition.getPropertyValues().getPropertyValues();
+        for (PropertyValue propertyValue : propertyValues) {
+            Field declaredField = bean.getClass().getDeclaredField(propertyValue.getName());
+            declaredField.setAccessible(true);
+            declaredField.set(bean, propertyValue.getValue());
+        }
     }
 }
